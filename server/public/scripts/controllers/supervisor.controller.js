b12bd6d7445bb93c065434c585ba304c5db494dc
Sarah Harrington <sarah.soberg@gmail.com>
2017-12-11 17:03:13 -0600

    working on calendar

--- server/public/scripts/controllers/supervisor.controller.js
+++ server/public/scripts/controllers/supervisor.controller.js
@@ -3,18 +3,29 @@ myApp.controller('SupervisorController', function(UserService, ShiftService, Ava
     console.log('SupervisorController created');
     var vm = this;
     vm.userService = UserService;
+    vm.shiftService = ShiftService;
     vm.userObject = UserService.userObject;
 
+    vm.shiftDetails = function (event) {
+      ShiftService.shiftDetails(event)
+    }
+  
+    vm.addShift = function (event) {
+      ShiftService.addShift(event)
+    }
 
-    vm.today = moment();
-    console.log(vm.today);
+    vm.today = moment().format('d');
+
+    console.log('today', vm.today);
 
     vm.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemeber', 'December'];
     
     vm.currentSchedule = {
       dates: []
     };
-    var scheduleDays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+  
+    var scheduleDays = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
     vm.nextSchedule = function(scheduleDays) {
       for (let i = 0; i < scheduleDays.length; i++) {
         vm.currentSchedule.dates.push(moment().add(scheduleDays[i], 'days'));
@@ -22,6 +33,8 @@ myApp.controller('SupervisorController', function(UserService, ShiftService, Ava
     }
     console.log((moment().day(4).format('d')));
     
+    vm.nextSchedule(scheduleDays);
+
     vm.checkToday = function() {
       if (vm.today === 4) {
         for (let i = 0; i < scheduleDays.length; i++) {
@@ -29,28 +42,17 @@ myApp.controller('SupervisorController', function(UserService, ShiftService, Ava
         }
       }
       else if (vm.today < 4) {
+        var priorDaystoGet = 5 - vm.today
+        console.log('priorDaystoGet', priorDaystoGet);
 
       }
       else if (vm.today > 4) {
         
       }
     }
+
+    vm.checkToday();
     
-    })
+  })
 
-myApp.controller('SupervisorController', function ($mdDialog, UserService, ShiftService, AvailabilityService) {
-  console.log('SupervisorController created');
-  var vm = this;
-  vm.userService = UserService;
-  vm.shiftService = ShiftService;
-  vm.userObject = UserService.userObject;
-
-  vm.shiftDetails = function (event) {
-    ShiftService.shiftDetails(event)
-  }
-
-  vm.addShift = function (event) {
-    ShiftService.addShift(event)
-  }
-});
 

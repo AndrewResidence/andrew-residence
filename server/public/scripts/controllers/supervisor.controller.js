7a6ac70f70f010ed1f3691d367d686f76a03ebee
Sarah Harrington <sarah.soberg@gmail.com>
2017-12-11 23:40:04 -0600

    added comments, made some styling updates

--- server/public/scripts/controllers/supervisor.controller.js
+++ server/public/scripts/controllers/supervisor.controller.js
@@ -14,80 +14,86 @@ myApp.controller('SupervisorController', function(UserService, ShiftService, Ava
       ShiftService.addShift(event)
     }
 
-    vm.month = moment().format('MMMM');
-    console.log(vm.month)
-    vm.today = parseInt(moment().format('d'));
-    console.log('today', vm.today);
+    //used for assigning month/day in the calendar header
+    vm.month = '';
+    vm.year = '';
+    vm.todayToday = moment();
 
+    //array for for the next schedules function dates
     vm.currentSchedule = {
       dates: []
     };
   
+    //gets the two week schedule
     var scheduleDays = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-    console.log((moment().day(4).format('d')));
-
+    vm.today = parseInt(moment().format('d'));
     vm.nextSchedule = function(scheduleDays) {
       var priorDaystoGet;
       var priorDays = [];
 
-      switch (vm.today) {
-        case 0:
-          priorDaystoGet = 3;
-          break;
-        case 1:
-          priorDaystoGet = 4;
-          break;
-        case 2:
-          priorDaystoGet = 5;
-          break;
-        case 3:
-          priorDaystoGet = 6;
-          break;
-        case 4:
-          priorDaystoGet = 0;
-          break;
-        case 5:
-          priorDaystoGet = 1;
-          break;
-        case 6:
-          priorDaystoGet = 2;
-          break;
-        default: console.log('boo')
-          break;
-      }
+        switch (vm.today) {
+          case 0:
+            priorDaystoGet = 3;
+            break;
+          case 1:
+            priorDaystoGet = 4;
+            break;
+          case 2:
+            priorDaystoGet = 5;
+            break;
+          case 3:
+            priorDaystoGet = 6;
+            break;
+          case 4:
+            priorDaystoGet = 0;
+            break;
+          case 5:
+            priorDaystoGet = 1;
+            break;
+          case 6:
+            priorDaystoGet = 2;
+            break;
+          default: console.log('boo')
+            break;
+        }
       priorDays = scheduleDays.map(function(v) {
         return v - priorDaystoGet;
       })
-      console.log('priorDays', priorDays)
+      
       for (var i = 0; i < priorDays.length; i++) {
         vm.currentSchedule.dates.push(moment().add(priorDays[i], 'days'));
       }
+
+      vm.month = moment(vm.todayToday).format('MMMM');
+      vm.year = moment(vm.todayToday).format('YYYY');
     }
+
+    //functionc all to get schedule on page load
     vm.nextSchedule(scheduleDays);
 
     //function to pull prior two weeks of dates
     vm.prevMonth = function(date) {
       vm.currentSchedule.dates = [];
       var prevTwoWeeks = moment(date).subtract(14, 'days');
-      console.log('previous two weeks', prevTwoWeeks._d)
       
       for (var i = 0; i < scheduleDays.length; i++) {
-        console.log('in the for loop')
         vm.currentSchedule.dates.push(moment(prevTwoWeeks._d).add(scheduleDays[i], 'days'));
       }
-      console.log('prev two weeks array', vm.currentSchedule.dates)
+
+      vm.month = moment(prevTwoWeeks._d).format('MMMM');
+      vm.year = moment(prevTwoWeeks._d).format('YYYY')
     }
 
+    //function to get next two weeks of dates
     vm.nextMonth = function(date) {
       vm.currentSchedule.dates = [];
-      console.log('next month clicked', date)
       var nextTwoWeeks = moment(date).add(14, 'days');
-      console.log('next two weeks', nextTwoWeeks._d);
 
       for (var i = 0; i < scheduleDays.length; i++) {
         vm.currentSchedule.dates.push(moment(nextTwoWeeks._d).add(scheduleDays[i], 'days'));
       }
+      vm.month = moment(nextTwoWeeks._d).format('MMMM');
+      vm.year = moment(nextTwoWeeks._d).format('YYYY');
     }
   })
 

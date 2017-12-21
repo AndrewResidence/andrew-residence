20137a40789f66b4f11f8a093ecf097afd0befc9
Emma Stout <emma.stout01@gmail.com>
2017-12-21 17:15:11 -0600

    sending all bidders on confirm staff route

--- server/public/scripts/services/shift.service.js
+++ server/public/scripts/services/shift.service.js
@@ -133,9 +133,9 @@ myApp.service('ShiftService', function ($http, $location, $mdDialog) {
     });
   };
 
-  self.confirmShift = function(staffMember) {
+  self.confirmShift = function(staffMember, allShifts) {
     console.log('staff member to confirm', staffMember.name);
-    return $http.post('/shifts/confirm', staffMember).then(function (response) {
+    return $http.post('/shifts/confirm', staffMember, allShifts).then(function (response) {
       console.log('confirmed shift', staffMember.name, response);
       return response;
     });

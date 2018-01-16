21ef5b542f3fcb8277bbda00831835be5bc7c501
Josh Nothum <joshnothum@gmail.com>
2018-01-15 19:09:52 -0600

    updated readme.md

--- server/public/scripts/controllers/staff.controller.js
+++ server/public/scripts/controllers/staff.controller.js
@@ -95,7 +95,7 @@ myApp.controller('StaffController', function (UserService, ShiftService, Availab
       clickOutsideToClose: true,
       locals: { shift: shift },
       fullscreen: self.customFullscreen // Only for -xs, -sm breakpoints.
-    });
+    }); 
   };
   
   //gets number of days in month

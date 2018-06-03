3841ddcd1bc5db784f8f8f66088c2953d1acc6b5
Sarah Harrington <sarah.soberg@gmail.com>
2018-06-02 23:59:16 -0500

    updated emails

--- server/routes/message.router.js
+++ server/routes/message.router.js
@@ -35,7 +35,7 @@ console.log('I can make logs!!');
 let weeklyDigestEmailArray = [];
 let weeklyDigestShiftsArray = [];
 //node-cron function to send weekly recap email
-var weeklyEmailTimer = cron.schedule('0 57 23 * * SAT', function () {
+var weeklyEmailTimer = cron.schedule('0 0 0 * * SUN', function () {
     console.log('cron job running');
     getEmailRecAndShifts();
 })
@@ -88,9 +88,9 @@ function weeklyDigestEmailSend(emails, shifts) {
                 {
                     to: newEmails,
                     subject: 'Weekly Digest from Andrew Residence',
-                    // substitutions: {
-                    //     shifts: availableShifts,
-                    // }
+                    substitutions: {
+                        shifts: availableShifts,
+                    }
                 },
             ],
             from: {
@@ -106,7 +106,7 @@ function weeklyDigestEmailSend(emails, shifts) {
                     value: 
                         ' <body>' +
                         '<h1>THIS EMAIL IS A TEST</h1>' +
-                        '<h1>Andrew Residence</h1><h3>Currently available on-call shifts:</h3>' + avail +
+                        '<h1>Andrew Residence</h1><h3>Currently available on-call shifts:</h3>' + shifts +
                         '<p>Please go to the scheduling app to sign-up for a shift.</p>' +
                         '<button style="background-color: #4CAF50;background-color:rgb(255, 193, 7);color: white;padding: 15px 32px;text-align: center;font-size: 16px;border-radius: 5px;border: none;" ><a href="https://andrew-residence.herokuapp.com/" style="text-decoration: none; color: white"/>Let\'s Pick-up Some Shifts!</button>' +
                         '<p> We appreciate yor support!</p></body>',

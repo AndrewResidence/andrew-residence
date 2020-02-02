09448c654627df8f75601ac4864beae22649b0e4
Sarah Harrington <sarah.soberg@gmail.com>
2020-02-02 09:51:08 -0600

    added phone validation to user edit and admin edit

--- server/public/scripts/controllers/adminDialog.controller.js
+++ server/public/scripts/controllers/adminDialog.controller.js
@@ -16,37 +16,15 @@ myApp.controller('AdminDialogController', function ($mdDialog, UserService, user
 
     //edit the user information
     vm.editUser = function (user) {
-        let tempPhoneNum = []
-        for (let i = 0; i < user.phone.length; i++) {
-            if (Number(user.phone[i])) {
-                tempPhoneNum.push(user.phone[i]);
-            }
-        }
 
-        if (tempPhoneNum.length > 11 || tempPhoneNum.length < 10) {
-            vm.phoneMessage = "Please enter your phone number including area code";
+        let phoneValidation = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/
+        let phoneArray = vm.user.phone.match(phoneValidation)
+
+        if (phoneArray === null) {
+            vm.phoneMessage = "Please enter your phone number in the format (xxx)xxx-xxxx"
             return
         }
-        if (tempPhoneNum.length === 11) {
-            if (parseInt(tempPhoneNum[0]) !== 1) {
-                vm.phoneMessage = "Please enter your 10 digit phone number including area code";
-                return
-            }
-            else {
-                user.phone = tempPhoneNum.join('');
-            }
-        }
-        if (tempPhoneNum.length === 10) {
-            if (parseInt(tempPhoneNum[0]) === 1 || parseInt(tempPhoneNum[0]) === 0) {
-                vm.phoneMessage = "Please enter your phone number including area code";
-                return
-            }
-            else {
-                tempPhoneNum.unshift('1');
-                user.phone = tempPhoneNum.join('');
-            }
-        }
-
+        vm.user.phone = "1" + phoneArray[1] + phoneArray[2] + phoneArray[3]
         vm.userService.editUser(user).then(function (response) {
             // console.log('edited user', response);
             $mdDialog.hide();

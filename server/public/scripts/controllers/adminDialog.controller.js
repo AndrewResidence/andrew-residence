d1a5ce444215f438eb7b26e93ca3988d7061797a
Sarah Harrington <sarah.soberg@gmail.com>
2020-03-21 21:25:01 -0500

    removing phone validation

--- server/public/scripts/controllers/adminDialog.controller.js
+++ server/public/scripts/controllers/adminDialog.controller.js
@@ -10,21 +10,21 @@ myApp.controller('AdminDialogController', function ($mdDialog, UserService, user
             console.log('confirmed user', response);
             $mdDialog.hide();
         }).catch(function (error) {
-            console.log('error in confirming user')
+            console.log('error in confirming user') 
         })
     };
 
     //edit the user information
     vm.editUser = function (user) {
         console.log('the save button was clicked', user)
-        let phoneValidation = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/
-        let phoneArray = vm.user.phone.match(phoneValidation)
+        // let phoneValidation = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/
+        // let phoneArray = vm.user.phone.match(phoneValidation)
 
-        if (phoneArray === null) {
-            vm.phoneMessage = "Please enter your phone number in the format (xxx)xxx-xxxx"
-            return
-        }
-        user.phone = "1" + phoneArray[1] + phoneArray[2] + phoneArray[3]
+        // if (phoneArray === null) {
+        //     vm.phoneMessage = "Please enter your phone number in the format (xxx)xxx-xxxx"
+        //     return
+        // }
+        // user.phone = "1" + phoneArray[1] + phoneArray[2] + phoneArray[3]
         console.log('the user before servicee', user)
         vm.userService.editUser(user).then(function (response) {
             console.log('edited user - in controller', response);
